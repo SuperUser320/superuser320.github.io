cd35dc2ae74aa0e17a1e889bd394508f27cd9a90
Jason Merlo <macman320@gmail.com>
2014-12-09 16:51:08 -0500

    Added animations and footer

--- js/fade.js
+++ js/fade.js
@@ -13,15 +13,13 @@ $(document).ready(function () {
 
         if (scrollDist <= limit * 2) {
             divs.css({'opacity': (1 - scrollDist / limit)});
-            divs.css({'-webkit-transform': 'translate3d(0,' + (1 - scrollDist / limit)} + ',0)');
         }
         
-        /* Check the location of each desired element */
+        /* Fade in elements*/
         $('.fadein').each(function (i) {
             var bottom_of_object = $(this).position().top + $(this).outerHeight() / 2, bottom_of_window = $(window).scrollTop();
-            /* If the object is completely visible in the window, fade it it */
             if (bottom_of_window > bottom_of_object) {
-                $(this).animate({'opacity': '1'}, 750);
+                $(this).css({'opacity': '1', 'transform': 'scale(1.0)'}, 750);
             }
         });
     });
